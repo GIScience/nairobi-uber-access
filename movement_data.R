d01a7b071ff92ed7ae97c404ec35c8598711b6ed
Oliver Fritz <oliver.fritz@heigit.org>
2023-09-26 11:00:43 +0200

    wip

--- movement_data.R
+++ movement_data.R
@@ -27,7 +27,7 @@ gp2sf <- function(gp) {
     pull(1)
 }
 
-if (!file.exists("nairobi_boundary.gpkg")) {
+if (!file.exists("data/nairobi_boundary.gpkg")) {
   nairobi <- opq(bbox = 'Nairobi, Kenya') |>
     add_osm_feature(key = 'name', value = 'Nairobi') |>
     add_osm_feature(key = 'admin_level', value = '3') |>
@@ -38,15 +38,15 @@ if (!file.exists("nairobi_boundary.gpkg")) {
   #library(rgeoboundaries)
   #nairobi_boundary <- gb_adm1("Kenya") |> filter(shapeName=="Nairobi")
 
-  nairobi_boundary |> st_write("nairobi_boundary.gpkg", append=F)
+  nairobi_boundary |> st_write("data/nairobi_boundary.gpkg", append=F)
 } else {
-  nairobi_boundary <- st_read("nairobi_boundary.gpkg")
+  nairobi_boundary <- st_read("data/nairobi_boundary.gpkg")
 }
 
 
 #kenya_wards <- gb_adm4("Kenya")
-kenya_wards <- st_read("Kenya_Wards/kenya_wards.shp")
-kenya_wards <- kenya_wards |>  st_make_valid()
+kenya_wards <- st_read("data/Kenya_Wards/kenya_wards.shp")
+kenya_wards <- kenya_wards |> st_make_valid()
 kenya_wards$centroid <- kenya_wards |> st_point_on_surface()
 
 nairobi_boundary_buff <- nairobi_boundary |> st_transform(32737) |> st_buffer(10000) |> st_transform(4326)
@@ -71,8 +71,8 @@ hex_grid <- hex_grid |> left_join(hex_grid_centroid |> data.frame() |> select(c(
 
 nrow(hex_grid)
 
-plot(hex_grid["ward"], lwd=.1)
-plot(nairobi_boundary |> st_geometry(), fill=, add=T)
+plot(hex_grid["ward"] |> st_geometry(), lwd=.1)
+plot(nairobi_boundary |> st_geometry(), add=T)
 
 #st_write(hex_grid, "hex_grid.gpkg", append=F)
 
@@ -81,15 +81,16 @@ tic.clear()
 # loop through hexgrid
 result <- data.frame(bbox=NA, geometry=NA, way_points=NA, legs=NA, departure=NA, arrival=NA, geom=NA, status=NA, id_origin=NA, id_destination=NA, distance=NA,duration=NA)
 
-tic("first 15")
-for (i in 1:15) {
+n = 15
+tic("first n")
+for (i in 1:n) {
 #for (i in 1:nrow(hex_grid)) {
   #i <- 1
   tic(glue("run {i} / {nrow(hex_grid)}"))
   origin <- hex_grid[i,]
 
   #for (t in nrow(hex_grid):1) {
-  for (t in nrow(hex_grid):1) {
+  for (t in nrow(hex_grid):i+1) {
     #t <- 264
     if (i == t) {next}
 
@@ -108,6 +109,7 @@ for (i in 1:15) {
          arrival="2018-02-23T06:00:00",
          instructions=F) |> jsonlite::toJSON(auto_unbox = T)
 
+    # check if POST is faster than VERB
     res <- VERB("POST", url = "http://localhost:8080/ors/v2/directions/driving-car", body = body, add_headers(headers))
     print(glue(" run i {i}; run {t}, {res$status_code}"))
     if (res$status_code!=200) {
@@ -115,6 +117,7 @@ for (i in 1:15) {
       df_resp <- data.frame(status=res$status_code)
       df_resp$id_origin <- i
       df_resp$id_destination <- t
+      # alternative: bind_rows
       result <- rbind.fill(result, df_resp)
       rm(res,df_resp)
       print(glue(" rows: {nrow(result)}"))
