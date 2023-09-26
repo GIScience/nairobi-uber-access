53f9672c2e4dc2afecf7fbba4ac88fc92f4384a2
Oliver Fritz <oliver.fritz@heigit.org>
2023-09-26 17:54:18 +0200

    use pmap and clean up

--- fun.R
+++ fun.R
@@ -1,12 +1,14 @@
+library(httr)
+library(sf)
+library(tidyverse)
+
 get_ors_response <- function(
         origin_coords, 
         dest_coords, 
         arrival, 
         url = "http://localhost:8080/ors/v2/directions/driving-car"
 ) {
-    headers = c(
-        'Content-Type' = 'application/json'
-    )
+    headers = c('Content-Type' = 'application/json')
     
     body <- list(
         coordinates = list(origin_coords, dest_coords),
@@ -17,34 +19,30 @@ get_ors_response <- function(
     res <- POST(url = url, body = body, add_headers(headers))
 }
 
-get_route <- function(row) {
-
-    origin_coord <- row[1,1] |> st_coordinates() |> as.numeric()
-    dest_coord <- row[1,2] |> st_coordinates() |> as.numeric()
-    arrival <- row[1,3]
-    
-    res <- get_ors_response(
-        origin_coords = origin_coord,
-        dest_coords = dest_coord,
-        arrival = arrival
-    )
-    
-    
+parse_ors_response <- function(res) {
     if (res$status_code != 200) {
-        
-        result <- data.frame(status=res$status_code)
-        
+        result <- data.frame(status = res$status_code)
     } else {
         json_resp <- res$content |> rawToChar() |> jsonlite::fromJSON()
-        result <- json_resp$routes |> as.data.frame()
-        result <- result |> mutate(geom = gp2sf(geometry)) |>
-            st_sf(crs = "EPSG:4326")
-        result$status <- res$status_code
-        result$distance <- result$summary$distance
-        result$duration <- result$summary$duration
-        result <- result |> select(-c(summary))
+        result <- json_resp$routes |> 
+            as.data.frame() |> 
+            mutate(
+                geom = gp2sf(geometry),
+                status = res$status_code
+            ) |>
+            st_sf(crs = "EPSG:4326") |>
+            unnest(summary)
     }
     result
+}
+
+get_route <- function(origin_coord, dest_coord, arrival) {
+    get_ors_response(
+        origin_coords = origin_coord |> st_coordinates() |> as.numeric(),
+        dest_coords = dest_coord |> st_coordinates() |> as.numeric(),
+        arrival = arrival
+    ) |>
+        parse_ors_response()
 }
 
 get_routes <- function(
@@ -54,15 +52,24 @@ get_routes <- function(
         dest_ids, 
         arrivals
 ) {
-    
-    stopifnot(length(origin_centroids) == length(origin_ids))
-    stopifnot(length(dest_centroids) == length(dest_ids))
+    stopifnot(
+        length(origin_centroids) == length(origin_ids),
+        length(dest_centroids) == length(dest_ids)
+    )
 
-    grid <- expand.grid(origin_centroids, dest_centroids, arrivals)
-    ids <- expand.grid(origin_id = origin_ids, dest_id = dest_ids, arrival = arrivals)
+    grid <- expand.grid(
+        origin_coord = origin_centroids, 
+        dest_coord = dest_centroids, 
+        arrival = arrivals
+    )
+    ids <- expand.grid(
+        origin_id = origin_ids, 
+        dest_id = dest_ids, 
+        requested_arrival = arrivals
+    )
     
-    split(grid, seq(nrow(grid))) |>
-        map(get_route) |>
+    grid |>
+        pmap(get_route) |>
         reduce(bind_rows) |>
-        cbind(ids)
+        bind_cols(ids)
 }
