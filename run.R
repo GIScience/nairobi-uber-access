3b34ccf8911fde5a8d3fb417e51f8f86e58f5d26
Marcel Reinmuth <marcel.reinmuth@stud.uni-heidelberg.de>
2023-09-29 16:40:50 +0200

    another update to run

--- run.R
+++ run.R
@@ -8,6 +8,8 @@
 library(tidyverse)
 library(sf)
 library(osmdata)
+library(tmap)
+library(ggplot2)
 source("fun.R")
 
 #2 Generate routes ---------------------------------
@@ -35,8 +37,8 @@ saveRDS(routes, paste0(period,"_", hex_grid |> nrow(), ",.rds"))
 #3 Assess output ---------------------------------
 
 
-semester_routes <- readRDS(paste0(period,"_", hex_grid |> nrow(), ",.rds"))
-holiday_routes <- readRDS(paste0(period,"_", hex_grid |> nrow(), ",.rds"))
+semester_routes <- readRDS(paste0("semester_", hex_grid |> nrow(), ".rds"))
+holiday_routes <- readRDS(paste0("holiday_", hex_grid |> nrow(), ".rds"))
 
 holiday_routes$cat <- "holiday"
 semester_routes$cat <- "semester"
@@ -50,7 +52,42 @@ holiday_routes$distance_diff <- holiday_routes$distance - semester_routes$distan
 routes <- bind_rows(holiday_routes,
                     semester_routes)
 
-routes_agg <- routes |> group_by(cat, requested_arrival, origin_id) |>
+#4 Non-spatial ---------------------------------
+
+routes <- routes |>
+  mutate(time=as.factor(case_when(
+    requested_arrival == "2019-04-11T08:00:00" ~ 8,
+    requested_arrival == "2019-04-11T16:00:00" ~ 16,
+  )))
+
+mu <- routes |> group_by(cat, time) |>
+  summarise(grp.mean = mean(duration_diff, na.rm=T))
+
+
+routes |> filter(cat=="semester") |>  ggplot(
+  aes(x = duration_diff,
+      color = time,
+      fill = time))+ geom_histogram(aes(y = ..density..),
+                                                 position = "identity", alpha = 0.5) +
+  geom_density(alpha = 0.6) +
+  geom_vline(
+    data = mu |> filter(cat=="semester"),
+    aes(xintercept = grp.mean, color = time),
+    linetype = "dashed"
+  ) +
+  labs(title = "Distance histogram", x = "Distance", y = "Density")
+
+# random pick of origin 40
+routes |>
+  filter(cat=="semester" & origin_id==140) |>
+  ggplot(aes(x=distance, y=duration_diff, group=time)) +
+  #geom_line(aes(color=time)) +
+  geom_smooth(aes(color=time)) +
+  theme_light()
+
+
+# lets do some aggregations
+routes_agg <- routes |> group_by(cat, time, origin_id) |>
   summarise(
     duration_diff_sum = sum(duration_diff, na.rm = T),
     duration_diff_avg = mean(duration_diff, na.rm = T),
@@ -61,16 +98,48 @@ routes_agg <- routes |> group_by(cat, requested_arrival, origin_id) |>
     duration_diff_q75 =quantile(duration_diff, probs = 0.75, na.rm = T)
   ) |> ungroup()
 
+
+ioi <- data.frame(bind_rows(
+  routes_agg |> filter(cat=="semester") |> arrange(duration_diff_avg) |> head(5) |> select(origin_id),
+  routes_agg |> filter(cat=="semester" & !is.nan(duration_diff_avg)) |> arrange(duration_diff_avg) |> tail(5) |> select(origin_id),
+  routes_agg |> filter(cat=="semester") |> arrange(abs(duration_diff_avg)) |> head(5) |> select(origin_id)
+), type=c(rep("avg.increase",5),
+          rep("avg.decrease",5),
+          rep("avg.neutral",5)))
+
+
+routes |> inner_join(ioi, by = c("origin_id"="origin_id"))
+
+
+routes |>
+  #filter(cat=="semester" & origin_id %in% ioi$origin_id) |>
+  filter(cat=="semester") |>
+  right_join(ioi, by = c("origin_id"="origin_id")) |>
+  ggplot(aes(x=distance, y=duration_diff, group=time)) +
+  #geom_line(aes(color=time)) +
+  #geom_point(aes(color=time)) +
+  geom_smooth(aes(color=time)) +
+  facet_grid(~origin_id, cols = 5) +
+  #facet_grid(cols =vars(origin_id), rows=vars(type)) +
+  theme_light()
+
+
+#5 Spatial ---------------------------------
+
+
+
 hex_grid_join <- hex_grid |> left_join(routes_agg, by = c("hex_id"="origin_id"))
+hex_grid_join |> filter(cat=="semester") |> nrow()
 
-
+st_write(hex_grid_join, "hex_grid_join172.gpkg", append=F)
 
 tm_shape(hex_grid_join) +
   tm_polygons(
-    "duration_diff_median",
+    "duration_diff_avg",
     palette = "BrBG",
-    n = 5,
-    breaks = c(-500, -50, 0, 50, 500),
+    #n = 5,
+    breaks = c(-500, -10, 0, 10, 500),
   ) +
-  tm_facets(by = c("cat", "requested_arrival"),
+  tm_layout(asp=1) +
+  tm_facets(by = c("cat", "time"),
             free.scales = F)
